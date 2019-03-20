824fa7d69df65f103a1c521dc85020b0aba69d4e
Logan Litman <LoganLitman@Logans-MacBook-Air.local>
2019-03-20 16:13:18 -0400

    fixing the dependency error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3030;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(_dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 http.listen(port, () => {
