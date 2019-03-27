e6772134f6b27078e816836bb0210ba8fa60372a
Logan Litman <LoganLitman@Logans-Air.fc.ca>
2019-03-27 13:04:35 -0400

    adding stuff

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ io.attach(server);
 //socket.io chap app stuff to follow
 
 io.on('connection', function(socket) { //this is the switchboard operator - it manages everything 
-    console.log('a user has connected');
+    console.log('a user has connected', socket); //this socket knows what connection it has to manage and is storing this
+   
+    socket.emit('connected', {sID: `${socket.id}`, message: 'new connection'} ); //sending back a message to the machine thats connecting
+
+    socket.on('chat message', function(msg) { //when you hit submit - it passes through this chat message
+        console.log('message: ', msg, 'socket:', socket.id);
+
+        io.emit('chat message', { id: `${socket.id}`, message: msg }); //emiting another message here
+    })
 
     socket.on('disconnect', function() {
         console.log('a user has disconnected');
--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,5 +1,40 @@
+import ChatMessage from './modules/chatMessage.js';
+
 const socket = io();
 
-socket.addEventListener('connect', logConnect);
+function logConnect({sID, message}) {
+    console.log(sID, message);
+    vm.socketID = sID; //making this the sID were getting from the server
+} //making sure were getting the data back
+
+function appendMessage(message) {
+    vm.message.push(message);
+}
+
+//creating a vue instance below
+const vm = new VTTCue({
+    data: {
+        socketID: "",
+        nickname: "",
+        message: "",
+        messages: []
+    },
+
+    methods: {
+        dispatchMessage() {
+            //emit message event from the client side
+            socket.emit('chat message', { content: this.message, name: this.nickname || "Anonymous"});
+         
+            //reset the mesage field
+            this.message = "";
+        }
+    },
+
+    components: { //registering and using our chat message
+        newMessage: ChatMessage
+    }
+}).$mount(`#app`); //mounting our vue instance
+
+socket.on('connected', logConnect); //the server is listening down the line on our connector
 socket.addEventListener('chat message', appendMessage);
 socket.addEventListener('disconnect', appendMessage);
