19d640e4b60525c2edc8cf483b959e26949c646f
Logan Litman <LoganLitman@Logans-Air.fc.ca>
2019-03-27 13:51:08 -0400

    atgosdnfv

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')();
 
 const port = process.env.PORT || 3030;
 
@@ -12,6 +11,26 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-http.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
+});
+
+io.attach(server);
+
+io.on('connection', function(socket) {
+    console.log('a user has connected');
+
+    socket.emit('connected', { sID: `${socket.id}`, message: 'new connection'} );
+
+    // listen for an incoming message from anyone connected to the app
+    socket.on('chat message', function(msg) {
+        console.log('message: ', msg, 'socket:', socket.id);
+
+        // send the message to everyone connected to the app
+        io.emit('chat message', { id: `${socket.id}`, message: msg });
+    })
+
+    socket.on('disconnect', function() {
+        console.log('a user has disconnected');
+    });
 });
